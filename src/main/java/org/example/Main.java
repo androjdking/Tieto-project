3b1be9e60e910e2ea93e78718e1f51aae5da3783
Tom <t.reimer.st@spseiostrava.cz>
2024-05-21 09:37:13 +0200

    Interface and Dependency Injection

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         SceneManager sm = new SceneManager();
         sm.loop();
     }
